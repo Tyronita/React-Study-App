85eb3eb14784b320719ec30eb4b2fdaac2dc25ef
Tyronita <eol21@bath.ac.uk>
2022-03-28 17:28:35 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Navbar from './navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+          <h1>Dashboard</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Study App</h1>
+            <div className="links">
+                <a href="/"> Analytics</a>
+                <a href="/"> Calendar</a>
+                <a href="/"> Stopwatch</a>
+                <a href="/"> Todo</a>
+                <a href="">Settings</a>
+            </div>
+        </nav>
+    )  
+}
+
+export default Navbar
