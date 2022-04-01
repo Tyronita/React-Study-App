9d32bab3570e0d0a44475a0b1693f138a7c8017d
bottlecollector <bottlecollector52@gmail.com>
2022-04-01 04:04:45 +0100

    need to fix bug in graphpage

--- src/fakedata/gen_fake_data.py
+++ src/fakedata/gen_fake_data.py
@@ -7,12 +7,15 @@ import random
 
 monthLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 monthDays = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30];
-data = {m:[] for m in monthLabels}
+subjects = ["Photograph", "Music", "Maths", "Computer Science", "Chemistry"]
 
-for m in monthLabels:
-    data[m] = [random.randint(0, 10) for i in range( monthDays[monthLabels.index(m)] )]
+data = {s:{m:[] for m in monthLabels} for s in subjects}
+
+for s in subjects:
+    for m in monthLabels:
+        data[s][m] = [random.randint(0, 10) for i in range( monthDays[monthLabels.index(m)] )]
 
 # with open("./fake_data.json", "w") as f:
     # json.dumps(data, f, indent=4)
 
-print(json.dumps(data, indent=4))
+print(f'export default {json.dumps(data, indent=4)}')
