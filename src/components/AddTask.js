b335207af394bfce3f48e4fd169cf6998c4fb304
bottlecollector <bottlecollector52@gmail.com>
2022-04-03 19:29:11 +0100

    added ruth's code

---
+++ src/components/AddTask.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {useState} from 'react'
+const AddTask = ({onAdd}) => {
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+  const [important, setImportant] = useState(false)
+  const [date, setDate] = useState('')
+  const [reminder, setReminder] = useState(false)
+  const [disabler, setDisabler] = useState(true)
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+
+    if(!title){
+      alert('Please enter the name of the task')
+      return
+    }
+
+    onAdd({title, text, important, date, reminder, disabler})
+
+    setTitle('')
+    setText('')
+    setImportant('')
+    setDate('')
+    setReminder('')
+    setDisabler('')
+  }
+
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+        <div className='form-control'>
+            <label>Task</label>
+            <input type='text' placeholder='Add Task' value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <label>Description</label>
+            <input type='text' placeholder='Description'value={text} onChange={(e) => setText(e.target.value)}/>
+            <label>Date/Time</label>
+            <input type='text' placeholder='Add Date/Time'value={date} onChange={(e) => {setDate(e.target.value); e.target.value !== '' ? setDisabler(false) : setDisabler(true)}}/>
+            <label>Reminder</label>
+            <input type='checkbox' value={reminder} checked={reminder} disabled={disabler} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+            <label>Important</label>
+            <input type='checkbox' value={important} checked={important} onChange={(e) => setImportant(e.currentTarget.checked)}/>
+            
+            <input type='submit' value='Save Task' className = 'btn btn-block'/>
+        </div>
+    </form>
+  )
+}
+
+export default AddTask
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,8 +1,62 @@
+//import logo from './logo.svg';
+import '../styles/todo.css';
+import Header from '../components/Header'
+import Tasks from '../components/Tasks'
+import React, {useState} from 'react'
+import AddTask from '../components/AddTask';
+
 export default function TodoPage() {
-    return (
-        <div style={{minHeight: "100vh"}}>
-            <h2>Todo</h2>
-        </div>
-    );
+  const [showAddTask, setShowAddTask] = useState(false)
+    const [tasks, setTasks] = useState<any[]>([
+     
+  ])
+
+    const addTask = (task: any) => {
+    const id = Math.floor(Math.random() *10000)
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+    const deleteTask = (id: any) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+
+    const isImportant = (task: any, id: any) =>{
+    if(task.id === id && task.important === true){
+      return('task_important');
+    }
+    else{
+      return('task');
+    }
+  }
+
+    const changeImportance = (task: any, id: any) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, important: !task.important} : task))
+    return(isImportant(task, id));
+  }
+
+    const changeReminder = (task: any, id: any) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    return(reminderOn(task, id));
+  }
+
+    const reminderOn = (task: any, id: any) => {
+    if(task.id === id && task.reminder === true){
+      return('task_reminder');
+    }
+    else{
+      return('task');
+    }
+  }
+
+  return (
+    
+    <div className="Container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={changeImportance} onImportant={isImportant} onReminder={reminderOn} onChangeReminder={changeReminder}/> : 'No tasks available'}
+    </div>
+  );
 }
 
